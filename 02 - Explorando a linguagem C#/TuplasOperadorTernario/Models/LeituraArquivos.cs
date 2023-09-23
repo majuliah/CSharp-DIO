387b653f4468fffedf520c308ed2392b02528331
majulha <majulialcarvalho@gmail.com>
2023-09-23 18:27:34 -0300

    tuplas, operadores ternarios e destrutores concluido

---
+++ 02 - Explorando a linguagem C#/TuplasOperadorTernario/Models/LeituraArquivos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace TuplasOperadorTernario.Models
+{
+    public class LeituraArquivos
+    {
+        public (bool sucesso, string[] linhas, int quantidadeLinhas) LerArquivo(string caminho)
+        {
+            try
+            {
+                string[] linhas = File.ReadAllLines(caminho);
+                //um método só retorna um valor. com tuplas, pode retornar mais de um valor
+                return (true, linhas, linhas.Length);
+            }
+            catch (Exception e)
+            {
+                return (false, new string[0], 0);
+            }
+        }
+
+        // public void Deconstruct(out string nome, out string sobrenome)
+        // {
+        //     nome = nome;
+        //     sobrenome = sobrenome;
+        // }
+    }
+}
---
+++ 02 - Explorando a linguagem C#/TuplasOperadorTernario/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TuplasOperadorTernario.Models;
+using static System.Console;
+
+namespace TuplasOperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tupla
+            ValueTuple<int, string, string, decimal> outraTupla = (1, "Majulha", "Carvalho", 1.57m);
+            
+            WriteLine(outraTupla);
+            WriteLine(outraTupla.Item1);
+            WriteLine(outraTupla.Item2);
+            WriteLine(outraTupla.Item3);
+            
+            //outra sintaxe para tuplas
+            (int Id, string Nome, string Sobrenome) tupla = (32, "Maria Júlia", "Carvalho");
+            WriteLine(tupla.Id);
+            WriteLine(tupla.Nome);
+            WriteLine(tupla.Sobrenome);
+            
+            //tuplas em métodos
+            //testando método com tuplas
+
+            LeituraArquivos arquivo = new LeituraArquivos();
+            var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo(@"C:\Users\majul\Documents\CSharp-Dio\02 - Explorando a linguagem C#\ExcessoesEcolecoes\Arquivos\leitura.txt\");
+
+            if (sucesso)
+            {
+                foreach (var linha in linhasArquivo)
+                {
+                    WriteLine($"Quantidade linhas arquivo: {quantidadeLinhas}");
+                    WriteLine(linha);
+                }
+            }
+            else
+            {
+                WriteLine("Não foi possível lero arquivo");
+            }
+            
+            //descarte de informações
+            var (sucesso1, linhasArquivo2, _) = arquivo.LerArquivo(@"C:\Users\majul\Documents\CSharp-Dio\02 - Explorando a linguagem C#\ExcessoesEcolecoes\Arquivos\leitura.txt\");
+            //basta substituir os dados que não serão usados por "_"
+            
+            //desconstrutor
+            //if ternário
+
+            int numero = 20;
+            
+            if (numero % 2 == 0)
+            {
+                WriteLine("Par");
+            }
+            else
+            {
+                WriteLine("Impar");
+            }
+
+            string resposta = numero % 2 == 0 ? "Par" : "Impar";
+
+
+        }
+    }
+}
