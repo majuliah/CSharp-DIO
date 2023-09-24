ee728e00a82ec19cfe2615ba319c26469d6c5407
majulha <majulialcarvalho@gmail.com>
2023-09-24 17:45:10 -0300

    iniciando tipos anônomos

---
+++ 02 - Explorando a linguagem C#/TiposEspeciais/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Newtonsoft.Json;
+using NuggetSerializarEatributos.Models;
+using static System.Console;
+
+namespace TiposEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Tipos Especiais no C#");
+            //nulos
+            
+            bool? desejaReceberEmail = null;
+
+            if (desejaReceberEmail.HasValue && desejaReceberEmail.Value)
+            {
+                WriteLine("usuário optou por receber email");
+            }
+            else
+            {
+                WriteLine("Usuário optou por não receber email");
+            }
+
+            string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+            List<Vendas> listaVendas = JsonConvert.DeserializeObject<List<Vendas>>(conteudoArquivo);
+
+            foreach (var item in listaVendas)
+            {
+                WriteLine($"Id: {item.Id}, Produto: {item.Nome}, Preço: {item.Valor}, Data: {item.DataVenda}, Desconto: {(item.Desconto.HasValue ? $"Deconto de: {item.Desconto}" : "")}");
+            }
+
+            //tipos anônimos
+            // fornece foma de encapsular modos somente leitura.
+
+            var tipoAnonimo = new { Nome = "Maju", Sobrenome = "Majulha", Altura = 1.80 };
+            WriteLine($"Nome: {tipoAnonimo.Nome}, Sobrenome: {tipoAnonimo.Sobrenome}, Altura: {tipoAnonimo.Altura}");
+            
+        }
+    }
+}
---
+++ 02 - Explorando a linguagem C#/TiposEspeciais/Vendas/Vendas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NuggetSerializarEatributos.Models
+{
+    public class Vendas
+    {
+        public int Id { get; set; }
+        
+        [JsonProperty("Nome_Produto")]
+        public string Nome { get; set; }
+        
+        public decimal Valor { get; set; }
+        public DateTime DataVenda { get; set; }
+        public decimal? Desconto { get; set; }
+        
+    }
+}
