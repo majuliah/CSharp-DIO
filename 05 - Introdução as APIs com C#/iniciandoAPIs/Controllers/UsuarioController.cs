35fd0cc607fafb5e8ad5e4ed3ab387386eed4dba
majulha <majulialcarvalho@gmail.com>
2023-10-15 17:14:50 -0300

    introdução a apis concluido

--- 05 - Introdução as APIs com C#/iniciandoAPIs/Controllers/UsuarioController.cs
+++ 05 - Introdução as APIs com C#/iniciandoAPIs/Controllers/UsuarioController.cs
@@ -17,5 +17,12 @@ namespace iniciandoAPIs.Controllers
 
             return Ok(obj);
         }
+
+        [HttpGet("Apresentar/{nome}")]
+        public ActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo!";
+            return Ok(new{mensagem});
+        }
     }
 }
