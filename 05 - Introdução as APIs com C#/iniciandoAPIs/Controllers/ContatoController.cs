7d885450f0aa828c3c9aa2a02a98061379e8ecf7
majulha <majulialcarvalho@gmail.com>
2023-10-21 22:25:35 -0300

    criando endpoint para update

--- 05 - Introdução as APIs com C#/iniciandoAPIs/Controllers/ContatoController.cs
+++ 05 - Introdução as APIs com C#/iniciandoAPIs/Controllers/ContatoController.cs
@@ -1,4 +1,5 @@
 using iniciandoAPIs.Context;
+using iniciandoAPIs.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iniciandoAPIs.Controllers
@@ -31,6 +32,25 @@ namespace iniciandoAPIs.Controllers
                 return NotFound();
             
             return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarId(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null)
+                return NotFound();
+            
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+            
         }
     }
 }
